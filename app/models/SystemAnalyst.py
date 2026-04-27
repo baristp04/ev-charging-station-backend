b38918f78140e87b27d7fc016fefdf2364306087
baristp04 <baristepe2004@yandex.com>
2026-04-27 16:36:37 +0300

    database and new models

---
+++ app/models/SystemAnalyst.py
@@ -0,0 +1,25 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+
+class SystemAnalyst(SQLModel, table=True):
+    __tablename__ = "systemanalyst"
+
+    analystID: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    email: str = Field(unique=True, index=True)
+    passwordHash: str
+
+    #
+    # If you look at the UML diagram, the System Analyst "monitors" the Charging Stations.
+    # Unlike the Operations Specialist who "manages" specific stations (which requires a direct database link or foreign key so we know who manages what),
+    # a System Analyst typically has global read-access to the entire network to generate revenue reports and view utilization stats .
+    # Because of this, we don't need to add a specific relationship list or foreign key to this model or the ChargingStation model.
+    # The analyst will just query the tables directly when those report endpoints are called.
+    #
+    #
+    #
+    #
+    #
+    #
+    #
---
+++ app/models/operationspecialist.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models.station import ChargingStation
+
+#ismi OperationsSpecialist olamadigi icin ozur dilerim büyük harfle yapinca alakasiz bir yerde error veriyordu
+
+class OperationsSpecialist(SQLModel, table=True):
+    __tablename__ = "operationspecialist"
+
+    operatorID: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    email: str = Field(unique=True, index=True)
+    phone: str
+    passwordHash: str
+
+    # Relationship: An Operations Specialist manages multiple Charging Stations
+    managed_stations: List["ChargingStation"] = Relationship(back_populates="operator")
